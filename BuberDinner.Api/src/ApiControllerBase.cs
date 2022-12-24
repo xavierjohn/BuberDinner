6fc0be4735bf000698f8a2e72f46c87f899e3769
Xavier <xavierjohn@hotmail.com>
2022-12-23 16:12:35 -0800

    AssemblyAttribute ApiController

--- BuberDinner.Api/src/ApiControllerBase.cs
+++ BuberDinner.Api/src/ApiControllerBase.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 [Route("[controller]")]
-[ApiController]
 [Authorize]
 public class ApiControllerBase : FunctionalDDDBase
 {
