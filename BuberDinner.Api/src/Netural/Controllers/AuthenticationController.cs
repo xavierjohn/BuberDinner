6b00eba12ba8f80aacf9ec1e3d131dc6f230ddfd
Xavier John <xavierjohn@hotmail.com>
2022-12-23 17:38:51 -0800

    Remove lamda from MapAsync

--- BuberDinner.Api/src/Netural/Controllers/AuthenticationController.cs
+++ BuberDinner.Api/src/Netural/Controllers/AuthenticationController.cs
@@ -22,13 +22,13 @@ public class AuthenticationController : ApiControllerBase
     public async Task<ActionResult<AuthenticationResponse>> Register(RegisterRequest request) =>
         await request.ToRegisterCommand()
         .BindAsync(command => _sender.Send(command))
-        .MapAsync(authResult => _mapper.Map<AuthenticationResponse>(authResult))
+        .MapAsync(_mapper.Map<AuthenticationResponse>)
         .FinallyAsync(result => MapToActionResult(result));
 
     [HttpPost("login")]
     public async Task<ActionResult<AuthenticationResponse>> Login(LoginRequest request) =>
         await request.ToLoginQuery()
         .BindAsync(command => _sender.Send(command))
-        .MapAsync(authResult => _mapper.Map<AuthenticationResponse>(authResult))
+        .MapAsync(_mapper.Map<AuthenticationResponse>)
         .FinallyAsync(result => MapToActionResult(result));
 }
