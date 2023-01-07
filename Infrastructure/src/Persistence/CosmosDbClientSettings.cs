c0d6d173ba7c1cb24a3c31764e285ad45043e3be
Xavier <xavierjohn@hotmail.com>
2023-01-07 11:03:36 -0800

    Enable DefaultAzureCredential  (Managed Identity)

---
+++ Infrastructure/src/Persistence/CosmosDbClientSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace BuberDinner.Infrastructure.Persistence;
+
+public class CosmosDbClientSettings
+{
+    public string AccountEndPoint { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the account key.
+    /// When null use DefaultAzureCredential  https://learn.microsoft.com/en-us/dotnet/api/azure.identity.defaultazurecredential
+    /// </summary>
+    public string? AuthKeyOrResourceToken { get; set; }
+}
